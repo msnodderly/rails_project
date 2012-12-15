632f84266d77c35a5294cd363f7fc9eb53cd5c60
Matthew Snodderly <msnodderly@gmail.com>
2012-12-14 17:20:00 -0800

    made a mess

--- app/models/comments.rb
+++
@@ -1,5 +0,0 @@
-class Comments < ActiveRecord::Base
-  attr_accessible :content
-
-  belongs_to :posts
-end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates :title,  presence: true
   validates :url,  presence: true
 
+  has_many :comments
 end
