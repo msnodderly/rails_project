41d98551e5d4501a81f01c7172bdc10ec2ce8f3f
Matthew Snodderly <msnodderly@gmail.com>
2012-12-29 13:01:42 -0800

    added post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,11 +4,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def show
     #@post = Post.find(params[:id])
     @post = Post.where(slug: params[:id]).first
+  end
+
+  def create
+    @post = Post.new(params[:post])
+    if @post.save 
+      @status = "success"
+    else 
+      @status = "failed"
+    end
+
+    
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 CourseProject::Application.routes.draw do
-  #  create all 7 get/post/put route types
+#  create all 7 get/post/put route types
 #  resources :posts
 
   root to: "posts#index"
 
   match 'post/:id' => 'posts#show', :as => :post
+  match 'submit' => 'posts#new'
+  match 'create' => 'posts#create'
 
 end
