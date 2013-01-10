2e008d6b95150872cac89f65ef938807f50ddaa3
Matthew Snodderly <msnodderly@gmail.com>
2013-01-09 23:24:49 -0800

    updated rails

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,3 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 CourseProject::Application.initialize!
 
-ActiveSupport::CoreExtensions::Hash::Conversions::XML_PARSING.delete('symbol')
-ActiveSupport::CoreExtensions::Hash::Conversions::XML_PARSING.delete('yaml')
