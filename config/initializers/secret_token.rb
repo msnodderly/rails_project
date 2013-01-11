309fa8094c5fbbf51d5e9f86fd4518d23a1c510a
Matthew Snodderly <msnodderly@gmail.com>
2013-01-10 18:07:11 -0800

    token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,6 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CourseProject::Application.config.secret_token = 'dc4af147007e40c4fa75a03872cd026b8c2f760211ce635ac52d660e4c569971bdaa7f74d663e0366a49966181c7b2edb8806fa2e23263ffd7c782d182fd746a'
+#CourseProject::Application.config.secret_token = 'dc4af147007e40c4fa75a03872cd026b8c2f760211ce635ac52d660e4c569971bdaa7f74d663e0366a49966181c7b2edb8806fa2e23263ffd7c782d182fd746a'
+CourseProject::Application.config.secret_token = '9b485391af86de1df518f2154817653fb0b5a8650668ebfdc2206124a93a379ec20c789777e165e026f37c5f7891cb4512eb5d2d35ab1ef035093527f8c95a83'
+
