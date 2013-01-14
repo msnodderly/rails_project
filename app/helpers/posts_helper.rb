6dbb05819d356727ba469e14751f45aa1ac3e8bd
Matthew Snodderly <msnodderly@gmail.com>
2013-01-13 18:18:05 -0800

    display_url helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+  def display_url(post)
+    if post.url.starts_with?("http://") or post.url.starts_with?("https://") 
+      return post.url
+    else 
+      return "http://" + post.url
+    end
+
+  end
+
 end
