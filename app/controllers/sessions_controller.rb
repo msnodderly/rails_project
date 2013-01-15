402a6609defc3b68b35653deb9e004a42bba92e4
Matthew Snodderly <msnodderly@gmail.com>
2013-01-14 23:45:15 -0800

    added users and sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user] = user
+      redirect_to:root, notice: "Welcome #{user.username}."
+    else
+      flash[:error] = "Incorrect username or password." 
+      render 'new'
+    end
+  end
+
+  def destroy
+      session[:user] = nil   
+      redirect_to :root, notice: "You have been logged out."
+  end
+end
