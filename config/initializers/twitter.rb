11211bb037d237bef8e6737c5a6083593812ece3
Matthew Snodderly <msnodderly@gmail.com>
2013-01-16 23:47:35 -0800

    Update config/initializers/twitter.rb

--- config/initializers/twitter.rb
+++ config/initializers/twitter.rb
@@ -1,7 +1,1 @@
-Twitter.configure do |config|
-  config.consumer_key = "1ckeD8Uw3CeV7LsAHDfA"
-  config.consumer_secret = "FhEr0N9Nbfsn4WCEsxJ4AZmCJmKlMp5Sz110hZsY"
-  config.oauth_token = "23498592-9Gq7Q6y0PMuktLPQ3NU99szs8eFezqV42f0TMbuG9"
-  config.oauth_token_secret = "8xX2fiTg1c0fbND9dc9NoFWXVxyydJr58qPMmmzro4E"
-end
 
